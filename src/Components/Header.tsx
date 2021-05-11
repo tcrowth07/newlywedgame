ee501d76aa9a949f66c2b1ddcf7875165459a922
Tyler Crowther <tcrowth07@TyBook.local>
2021-05-11 14:15:38 -0600

    changed title

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,13 +10,13 @@ export default function Navbar({ fixed }: any) {
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-400 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
               className="text-lg font-bold leading-relaxed inline-block mx-4 py-2 whitespace-nowrap uppercase text-white"
               to="/"
             >
-              Newly-Wed Game
+              Newly-Wed Game <span className="text-gray-700">Online</span>
             </Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
